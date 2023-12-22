f461486af8feca0d807e50418004716186938e10
brianush1 <brianush1@outlook.com>
2023-12-22 14:44:27 -0500

    cleanup

--- examples/opengl/source/app.d
+++ examples/opengl/source/app.d
@@ -5,14 +5,21 @@ import gd.bindings.gl;
 
 void main() {
 	WindowInitOptions options;
-	options.title = "Basic demo";
-	options.initialState = WindowState.Visible;
+	options.title = "Basic OpenGL demo";
+	options.initialState = WindowState.Maximized;
+
 	Window win = application.display.createWindow(options);
+	scope (exit)
+		win.state = win.state | WindowState.Visible;
+
 	win.setPaintHandler({
 		GL.clearColor(1, 0, 1, 1);
 		GL.clear(GL.COLOR_BUFFER_BIT);
 	});
+
 	win.onCloseRequest.connect({
+		// the application will automatically terminate once no more events can occur,
+		// so we don't have to manually call quitApp()
 		win.state = win.state & ~WindowState.Visible;
 	});
 }
