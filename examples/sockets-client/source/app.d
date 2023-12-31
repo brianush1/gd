a6e0510094ab243a85eea31ada089ea8b85afd6c
brianush1 <brianush1@outlook.com>
2023-12-31 17:09:12 -0500

    add more examples

---
+++ examples/sockets-client/source/app.d
@@ -0,0 +1,20 @@
+import std.stdio;
+import gd.net.address;
+import gd.net.tcp;
+import gd.net.udp;
+import gd.timer;
+
+void main() {
+	UdpSocket udp = new UdpSocket("::0", 0);
+	udp.sendTo(resolveAddress("::0", 8091)[0].address, "Hello!");
+	writeln("Sent packet.");
+	udp.onReceive.connect((Address address, ubyte[] buffer) {
+		writeln("Received packet from ", address.toString, ":");
+
+		char[] msg = cast(char[]) buffer;
+		while (msg[$ - 1] == '\n')
+			msg = msg[0 .. $ - 1];
+
+		writeln(msg);
+	});
+}
